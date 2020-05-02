297d00281d2cb02965ab7ce9c40164a1d7bb9e5f
Dionysios Diamantopoulos @ vm on laptop <did@zurich.ibm.com>
2020-05-02 18:36:27 +0200

    Added doc/LinkDef.h

---
+++ doc/LinkDef.h
@@ -0,0 +1,11 @@
+/** \defgroup cFp_Vitis cloudFPGA Project Vitis (cFp_Vitis)
+ *
+ * \brief This is the peoject of cloudFPGA that bridges cFDK with Vitis Vision Open Source Library
+ */
+
+
+
+/** \defgroup Harris Harris Computer Vision Accelerated
+ *
+ * \brief This is the Harris accelerated function from Vitis Vision Open Source Library
+ */
