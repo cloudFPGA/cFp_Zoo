b3899b16907d37c6c0f8e6d87b5bceb6d2338bf1
Dionysios Diamantopoulos @ vm on laptop <did@zurich.ibm.com>
2020-06-04 20:31:43 +0200

    Added test/512x512.png.

--- ROLE/1/host/harris/include/config.h
+++ ROLE/1/host/harris/include/config.h
@@ -1,7 +1,7 @@
 
 
-#define FRAME_HEIGHT 8
-#define FRAME_WIDTH 8
+#define FRAME_HEIGHT 512
+#define FRAME_WIDTH 512
 #define FRAME_INTERVAL (1000/30)
 #define PACK_SIZE 1024 // This is our custom MTU. We must use a multiple of 8 (Bytes per transaction)! 1450 4086 udp pack size; note that OSX limits < 8100 bytes
 #define BUF_LEN 65540 // Larger than maximum UDP packet size
