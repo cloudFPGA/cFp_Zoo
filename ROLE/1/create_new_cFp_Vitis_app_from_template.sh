bcf6f0fc7473f75de067e5a3892a7df5118b0f7a
Dionysios Diamantopoulos @ vm on laptop <did@zurich.ibm.com>
2020-06-24 13:09:11 +0200

    Fixed script for generating kernels. [ci skip]

--- ROLE/1/create_new_cFp_Vitis_app_from_template.sh
+++ ROLE/1/create_new_cFp_Vitis_app_from_template.sh
@@ -1,33 +1,44 @@
 #/bin/sh
+# @author DID
 
 template_kernel="harris"
 new_kernel="safe"
 
-#echo "Copying folder " ${template_kernel} "to" ${new_kernel}
-#cp -r ./hls/${template_kernel} "to" ./hls/${new_kernel}
+function replace() {
+    files="$(find -L "$1" -type f)";
+    if [[ "$files" == "" ]]; then
+        echo "No files";
+        return 0;
+    fi
+    file_count=$(echo "$files" | wc -l)
+    echo "Count: $file_count"
+    echo "$files" | while read filen; do
+        if [[ $filen =~ "hls_reports" ]] || [[ $filen =~ "build" ]] 
+        then
+           echo "Skipping auxililiary file " ${filen}
+           continue
+        else
+           echo "Replacing in file " ${filen}
+        fi        
+        rpl ${template_kernel} ${new_kernel} ${filen}
+    done
+}
 
 
 find ./ -type d -name ${template_kernel} -print 2>/dev/null | while read dir_src_kernel; do
+    echo  "#####################################"
     parentdir="$(dirname "$dir_src_kernel")"
     newdir=${parentdir}"/"${new_kernel} 
     echo  "Copying dir: " ${dir_src_kernel} " to " ${newdir}
     cp -rf ${dir_src_kernel} ${newdir}
-    echo "Replacing file names in diectory " ${newdir}
+    echo "Replacing file names in directory " ${newdir}
     pattern="*"${template_kernel}"*"
     echo "Finding in " ${newdir} "for " ${pattern}
     find ${newdir} -type f -name ${pattern} | while read FILE ; do
+	echo "ranamed harris to safe in " ${FILE}
 	rename 's/harris/safe/g' ${FILE}
     done
-    new_pattern="*"${new_kernel}"*"
-    find ${newdir} -type f -name ${new_pattern} | while read FILEN ; do
-	echo "Replacing strings in file " ${FILEN}
-	rpl ${template_kernel} ${new_kernel} ${FILEN}  
-    done
+    echo "Replacing recursively in the files of directory " ${newdir}
+    replace ${newdir}
 done 
 
-#find . -type f -name ${template_kernel} | while read FILE ; do
-    #newfile="$(echo ${FILE} |sed -e 's/\\harris/safe/')" ;
-    #find . -type f -exec rename -n 's/'${template_kernel}'/'${new_kernel}'/g' {} +
-    #rename -n 's/harris/safe/g' *.jpg
-    #echo "from" ${FILE} "to" ${newfile}
-    #mv "${FILE}" "${newfile}" ;
