ab0bec16d127ea5795151e09b96430cc3bd7d45c
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-05-07 11:29:48 +0200

    Updates after demos (vmax).

--- HOST/custom/uppercase/languages/python/test.py
+++ HOST/custom/uppercase/languages/python/test.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-trieres_lib=os.environ['cFpRootDir'] + "/HOST/languages/python/build"
+trieres_lib=os.environ['cFpRootDir'] + "/HOST/custom/uppercase/languages/python/build"
 sys.path.append(trieres_lib)
 
 import _trieres
@@ -9,7 +9,7 @@ import _trieres
 input = "HelloWorld"
 output = "111111111"
 
-out, output = _trieres.uppercase("localhost", "1234", input, True)
+out, output = _trieres.uppercase("127.0.0.1", "1234", input, True)
 
 print(out)
 print(output)
--- HOST/quantitative_finance/mceuropeanengine/languages/python/test.py
+++ HOST/quantitative_finance/mceuropeanengine/languages/python/test.py
@@ -6,23 +6,24 @@ sys.path.append(trieres_lib)
 
 import _trieres
 
+
 loop_nm = 2;
 seed = 4332;
-underlying = 36;
-volatility = 0.20;
-dividendYield = 0.0;
-riskFreeRate = 0.06;
+underlying = 202.56481854952224;
+volatility = 0.005002151044533387;
+dividendYield = 0.011160536907441138;
+riskFreeRate = 0.008250879551073578;
 timeLength = 1;
-strike = 40;
+strike = 185.09682372241906;
 optionType = 1;
 requiredTolerance = 0.02;
-requiredSamples = 1;
+requiredSamples = 100000;
 timeSteps = 1;
 maxSamples = 1;    
     
 out = np.array([1.0,2.0]);
     
-out = _trieres.mceuropeanengine(loop_nm, "10.12.200.32", "2718",
+out = _trieres.mceuropeanengine(loop_nm, "10.12.200.144", "2718",
                             seed,
                             underlying,
                             volatility,
