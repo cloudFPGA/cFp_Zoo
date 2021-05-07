48898c14afdd47b280db3563d036846b826c3ae7
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-05-07 15:57:59 +0200

    Updated Vitis libs to fe06ac0.

--- HOST/quantitative_finance/mceuropeanengine/languages/python/test.py
+++ HOST/quantitative_finance/mceuropeanengine/languages/python/test.py
@@ -7,7 +7,7 @@ sys.path.append(trieres_lib)
 import _trieres
 
 
-loop_nm = 2;
+loop_nm = 1;
 seed = 4332;
 underlying = 202.56481854952224;
 volatility = 0.005002151044533387;
@@ -15,7 +15,7 @@ dividendYield = 0.011160536907441138;
 riskFreeRate = 0.008250879551073578;
 timeLength = 1;
 strike = 185.09682372241906;
-optionType = 1;
+optionType = 0;
 requiredTolerance = 0.02;
 requiredSamples = 100000;
 timeSteps = 1;
@@ -23,7 +23,7 @@ maxSamples = 1;
     
 out = np.array([1.0,2.0]);
     
-out = _trieres.mceuropeanengine(loop_nm, "10.12.200.144", "2718",
+out = _trieres.mceuropeanengine(loop_nm, "localhost", "2718",
                             seed,
                             underlying,
                             volatility,
