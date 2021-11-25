eabcc54be017e286f2f5af087b1985e643edc120
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-11-26 00:28:40 +0100

    Updating doc/LinkDef.h with cFp parent.

--- doc/LinkDef.h
+++ doc/LinkDef.h
@@ -1,25 +1,39 @@
-/** \defgroup cFp_Vitis cFp_Vitis
+/*****************************************************************************
  *
- * \brief This is the peoject of cloudFPGA that bridges cFDK with Vitis Vision Open Source Library.
+ *  cFDK / cFp : Submodules
+ *
+ *****************************************************************************/
+
+/** \defgroup cFp_Zoo cFp_Zoo
+ *  @ingroup cFp
+ * 
+ *  \brief This project is based on Themisto Shell and it features several domain-specific accelerators.
  */
 
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo : Submodules
+ *
+ *****************************************************************************/
+
 /** \defgroup VitisVision cF Vitis Vision
- *  @ingroup cFp_Vitis
+ *  @ingroup cFp_Zoo
  *
  * \brief This is the accelerated functions of Vitis Vision sub-library from Vitis Open Source Library.
  */
 
 /** \defgroup VitisQuantitativeFinance cF Vitis Quantitative Finance
- *  @ingroup cFp_Vitis
+ *  @ingroup cFp_Zoo
  *
  * \brief This is the accelerated functions of Vitis Quantitative Finance sub-library from Vitis Open Source Library.
  */
 
 
-
-
-
-//*********** VitisVision *****************************
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo / VitisVision : Submodules
+ *
+ *****************************************************************************/
 
 
 //*********** Harris*****************************
@@ -63,12 +77,11 @@
  */
 
 
-
-
-
-
-
-//*********** VitisQuantitativeFinance *****************************
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo / VitisQuantitativeFinance : Submodules
+ *
+ *****************************************************************************/
 
 
 //*********** MCEuropeanEngine*****************************
