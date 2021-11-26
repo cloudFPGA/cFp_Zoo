1c8c6424e432f8810aa5852e2412d3bf95414904
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-11-26 10:00:09 +0100

    Updated doc/LinkDef.h with the kernels up to now.

--- doc/LinkDef.h
+++ doc/LinkDef.h
@@ -28,6 +28,11 @@
  * \brief This is the accelerated functions of Vitis Quantitative Finance sub-library from Vitis Open Source Library.
  */
 
+/** \defgroup IBMZRL cF IBMZRL essentials
+ *  @ingroup cFp_Zoo
+ *
+ * \brief This is a set of projects developed by IBM Research Zurich Lab. The group contains educational projects and platform-stressing ones. 
+ */
 
 /*****************************************************************************
  *
@@ -46,7 +51,7 @@
 /** \defgroup HarrisTB Harris Testbench
  *  @ingroup Harris
  *
- * \brief This is a subgroup of Harris accelerated function with only testbench-related functions/classes/.
+ * \brief This is a subgroup of Harris accelerated function with only testbench-related functions/classes.
  */
 
 /** \defgroup HarrisHLS Harris Vivado HLS
@@ -55,6 +60,11 @@
  * \brief This is a subgroup of Harris accelerated function with only synthesizable (Vivado HLS) functions/classes.
  */
 
+/** \defgroup HarrisHost Harris Host
+ *  @ingroup Harris
+ *
+ * \brief This is a subgroup of Harris accelerated function with only host code software.
+ */
 
 
 //*********** Gammacorrection ********************
@@ -67,13 +77,47 @@
 /** \defgroup GammacorrectionTB Gammacorrection Testbench
  *  @ingroup Gammacorrection
  *
- * \brief This is a subgroup of Gammacorrection accelerated function with only testbench-related functions/classes/.
+ * \brief This is a subgroup of Gammacorrection accelerated function with only testbench-related functions/classes.
  */
 
 /** \defgroup GammacorrectionHLS Gammacorrection Vivado HLS
  *  @ingroup Gammacorrection
  *
  * \brief This is a subgroup of Gammacorrection accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+/** \defgroup GammacorrectionHost Gammacorrection Host
+ *  @ingroup Gammacorrection
+ *
+ * \brief This is a subgroup of Gammacorrection accelerated function with only host code software.
+ */
+
+
+
+
+//*********** MedianBlur ********************
+/** \defgroup MedianBlur MedianBlur
+ *  @ingroup VitisVision
+ *
+ * \brief This is the MedianBlur accelerated function from Vitis Vision Open Source Library.
+ */
+
+/** \defgroup MedianBlurTB MedianBlur Testbench
+ *  @ingroup MedianBlur
+ *
+ * \brief This is a subgroup of MedianBlur accelerated function with only testbench-related functions/classes.
+ */
+
+/** \defgroup MedianBlurHLS MedianBlur Vivado HLS
+ *  @ingroup MedianBlur
+ *
+ * \brief This is a subgroup of MedianBlur accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+/** \defgroup MedianBlurHost MedianBlur Host
+ *  @ingroup MedianBlur
+ *
+ * \brief This is a subgroup of MedianBlur accelerated function with only host code software.
  */
 
 
@@ -94,12 +138,77 @@
 /** \defgroup MCEuropeanEngineTB MCEuropeanEngine Testbench
  *  @ingroup MCEuropeanEngine
  *
- * \brief This is a subgroup of MCEuropeanEngine accelerated function with only testbench-related functions/classes/.
+ * \brief This is a subgroup of MCEuropeanEngine accelerated function with only testbench-related functions/classes.
  */
 
 /** \defgroup MCEuropeanEngineHLS MCEuropeanEngine Vivado HLS
  *  @ingroup MCEuropeanEngine
  *
  * \brief This is a subgroup of MCEuropeanEngine accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+/** \defgroup MCEuropeanEngineHost MCEuropeanEngine Vivado Host
+ *  @ingroup MCEuropeanEngine
+ *
+ * \brief This is a subgroup of MCEuropeanEngine accelerated function with only host code software.
+ */
+
+
+/*****************************************************************************
+ *
+ *  cFDK / cFp / cFp_Zoo / IBMZRL : Submodules
+ *
+ *****************************************************************************/
+
+
+//*********** Uppercase *****************************
+/** \defgroup Uppercase Uppercase
+ *  @ingroup IBMZRL
+ *
+ * \brief This is the Uppercase educational example. The FPGA accepts a string and returns the upper-case converted one.
+ */
+
+/** \defgroup UppercaseTB Uppercase Testbench
+ *  @ingroup Uppercase
+ *
+ * \brief This is a subgroup of Uppercase accelerated function with only testbench-related functions/classes.
+ */
+
+/** \defgroup UppercaseHLS Uppercase Vivado HLS
+ *  @ingroup Uppercase
+ *
+ * \brief This is a subgroup of Uppercase accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+/** \defgroup UppercaseHost Uppercase Host
+ *  @ingroup Uppercase
+ *
+ * \brief This is a subgroup of Uppercase accelerated function with only host code software.
+ */
+
+
+//*********** Memtest *****************************
+/** \defgroup Memtest Memtest
+ *  @ingroup IBMZRL
+ *
+ * \brief This is the Memtest platform stressing project. The FPGA tests its DRAM memory.
+ */
+
+/** \defgroup MemtestTB Memtest Testbench
+ *  @ingroup Memtest
+ *
+ * \brief This is a subgroup of Memtest accelerated function with only testbench-related functions/classes.
+ */
+
+/** \defgroup MemtestHLS Memtest Vivado HLS
+ *  @ingroup Memtest
+ *
+ * \brief This is a subgroup of Memtest accelerated function with only synthesizable (Vivado HLS) functions/classes.
+ */
+
+/** \defgroup MemtestHost Memtest Host
+ *  @ingroup Memtest
+ *
+ * \brief This is a subgroup of Memtest accelerated function with only host code software.
  */
 
