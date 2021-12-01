725c68b5d6436e703ab3371b0600be4823f33d19
DavideConficconi <dco@zurich.ibm.com>
2021-12-01 19:40:36 +0100

    [CONFIG] fix the script errors

--- create_cfp_json.sh
+++ create_cfp_json.sh
@@ -12,12 +12,12 @@ echo "    \"cFpSRAtype\": \"Themisto\"," >> cFp.json
 echo "    \"usedRoleDir\": \"$1/\"," >> cFp.json
 echo "    \"usedRoleDir2\": \"to-be-defined\"," >> cFp.json
 echo "    \"roleName1\": \"$1\"," >> cFp.json
-echo "    \"roleName2\": \"unused\"" >> cFp.json
+echo "    \"roleName2\": \"unused\"," >> cFp.json
 echo "    \"srat-conf\": {">> cFp.json
 echo "        \"version\": 0.3,">> cFp.json
 echo "        \"roles\": [">> cFp.json
 echo "        {">> cFp.json
-echo "                \"name\": "default",">> cFp.json
+echo "                \"name\": \"default\",">> cFp.json
 echo "                \"path\": \"\"">> cFp.json
 echo "            },">> cFp.json
 echo "            {">> cFp.json
