a8182340c9a1dc8e25a4c5388657bcd1ea638cda
Dionysios Diamantopoulos <did@zurich.ibm.com>
2021-12-22 14:39:29 +0100

    Added correct license header and attributed 3rd party copyrights.

--- HOST/common/langs/cpp/include/ologger.hpp
+++ HOST/common/langs/cpp/include/ologger.hpp
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Copyright 2016 -- 2022 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*******************************************************************************/
+
+/*
+ * Copyright 2020, arcomber, https://codereview.stackexchange.com/questions/254134/cout-style-logger-in-c
+ */
+
 /*
 ologger or ostream logger, a logger as convenient as using std::cout
 Features:
